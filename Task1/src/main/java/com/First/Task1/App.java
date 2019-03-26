f3c54c01da50a8fe26ef4f58c77f0fc337d4c830
lasya <lasya@192.168.0.3>
2019-03-26 06:39:08 +0530

    initial commit

---
+++ Task1/src/main/java/com/First/Task1/App.java
@@ -0,0 +1,17 @@
+package com.First.Task1;
+
+/**
+ * 
+ * @author Gajavelli Lasya
+ * 
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
