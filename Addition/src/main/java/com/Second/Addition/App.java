cfe2dc17d64c098b8b25740fe942f3db440a7a99
lasya <lasya@192.168.0.3>
2019-03-26 06:55:04 +0530

    second commit

---
+++ Addition/src/main/java/com/Second/Addition/App.java
@@ -0,0 +1,15 @@
+package com.Second.Addition;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=10;
+        int b=100;
+        System.out.println(a+b);
+    }
+}
